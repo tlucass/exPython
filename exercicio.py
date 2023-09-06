1a361ae46977c0629cb2c17166b1f10a5aeb2865
tlucass <rm97913@fiap.com.br>
2023-09-06 16:39:43 -0300

    exercicio 3

--- exercicio.py
+++ exercicio.py
@@ -5,12 +5,28 @@ ditado = input('Digite um ditado popular: ')
 nomeArq = input('Digite um nome para o arquivo: ')
 
 try:
-    arqCriado = open(f'd:/{nomeArq}.txt', 'w')
+    arqCriado = open(f'd:/{nomeArq}.txt', 'w', encoding='utf-8')
     arqCriado.write(ditado)
     arqCriado.close()
 
     arqCriado = open(f'd:/{nomeArq}.txt', 'r')
     print(arqCriado.read())
     arqCriado.close()
+except FileNotFoundError:
+    print('Arquivo não encontrado!')
+
+
+
+
+ditado2 = input('Agora, digite um outro ditado para adicionar ao arquivo já criado: ')
+
+try:
+    outroDitado = open(f'd:/{nomeArq}.txt', 'a', encoding='utf-8')
+    outroDitado.write(f'\n{ditado2}')
+    outroDitado.close()
+
+    outroDitado = open(f'd:/{nomeArq}.txt', 'r')
+    print(outroDitado.read())
+    outroDitado.close()
 except FileNotFoundError:
     print('Arquivo não encontrado!')
---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+numerosPares = {}
+
+while True:
+    nums = int(input("Digite números inteiros (pressione (0) para parar.): "))
+    numerosPares.add(nums)
+    if nums == 0:
+        break
