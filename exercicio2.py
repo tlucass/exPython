281bde6656383148ba369e75a6cd3b76eac42776
tlucass <rm97913@fiap.com.br>
2023-09-08 22:17:56 -0300

    exercicio 3

--- exercicio2.py
+++ exercicio2.py
@@ -1,7 +1,43 @@
-numerosPares = {}
+#Lucas P. de Toledo
+#RM: 97913
+
+import os
+
+numerosPares = {0}
+numerosPares.remove(0)
+
+if os.path.exists('d:/numPares.txt'):
+    os.remove('d:/numPares.txt')
+
+arquivo = open('d:/numPares.txt', 'x', encoding='utf-8')
+arquivo.close()
 
 while True:
-    nums = int(input("Digite números inteiros (pressione (0) para parar.): "))
-    numerosPares.add(nums)
-    if nums == 0:
+    try:
+        nums = int(input("Digite números inteiros (pressione (0) para parar.): "))
+        numerosPares.add(nums)
+    
+
+        if nums % 2 == 0:
+            pares = nums
+            if pares != 0:
+                try:
+                    arquivo = open('d:/numPares.txt', 'a', encoding='utf-8')
+                    arquivo.write(f"{str(pares)}\n")
+                    arquivo.close()
+                except FileNotFoundError:
+                    print('Arquivo não encontrado!')
+    
+
+        if nums == 0:
+            numerosPares.remove(0)
+            print(f"Set:{numerosPares}")
+
+            arquivo = open('d:/numPares.txt', 'r')
+            print(arquivo.read())
+            arquivo.close()
+            break
+
+    except ValueError:
+        print("Digite um valor válido!")
         break
